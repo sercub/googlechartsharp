8eeb758b7dfad8648a1c2097f378812af68943aa
lewisvance <lewisvance@2f2d1df1-4e41-0410-8a8b-3573fbf17017>
2007-12-16 17:41:15 +0000

    added ChartAxisType comments

--- trunk/ googlechartsharp/GoogleChartSharp/ChartAxis.cs
+++ trunk/ googlechartsharp/GoogleChartSharp/ChartAxis.cs
@@ -236,9 +236,24 @@ namespace GoogleChartSharp
     /// </summary>
     public enum ChartAxisType
     {
+        /// <summary>
+        /// Bottom x-axis
+        /// </summary>
         Bottom,
+
+        /// <summary>
+        /// Top x-axis
+        /// </summary>
         Top,
+
+        /// <summary>
+        /// Left y-axis
+        /// </summary>
         Left,
+
+        /// <summary>
+        /// Right y-axis
+        /// </summary>
         Right
     }
 }
