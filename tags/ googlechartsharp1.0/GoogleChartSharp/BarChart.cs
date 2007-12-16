a464b49d91a9c11c0fe1874a04e3661e086ab603
lewisvance <lewisvance@2f2d1df1-4e41-0410-8a8b-3573fbf17017>
2007-12-16 09:36:59 +0000

    Release 1.0

---
+++ tags/ googlechartsharp1.0/GoogleChartSharp/BarChart.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleChartSharp
+{
+    public enum BarChartOrientation
+    {
+        Vertical,
+        Horizontal
+    }
+
+    public enum BarChartStyle
+    {
+        Stacked,
+        Grouped
+    }
+
+    public class BarChart : Chart
+    {
+        BarChartOrientation orientation;
+        BarChartStyle style;
+        int barWidth;
+
+        public BarChart(int width, int height, BarChartOrientation orientation, BarChartStyle style)
+            : base(width, height)
+        {
+            this.orientation = orientation;
+            this.style = style;
+        }
+
+        public override string urlChartType()
+        {
+            char orientationChar = this.orientation == BarChartOrientation.Horizontal ? 'h' : 'v';
+            char styleChar = this.style == BarChartStyle.Stacked ? 's' : 'g';
+
+            return String.Format("b{0}{1}", orientationChar, styleChar);
+        }
+
+        public void SetBarWidth(int width)
+        {
+            this.barWidth = width;
+        }
+
+        protected override void collectUrlElements()
+        {
+            base.collectUrlElements();
+            if (this.barWidth != 0)
+            {
+                base.urlElements.Enqueue(String.Format("chbh={0}", this.barWidth));
+            }
+        }
+
+        public override ChartType getChartType()
+        {
+            return ChartType.BarChart;
+        }
+    }
+}
